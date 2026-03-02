05985d535b014d3c76e370eb7a1b49e1a118d2ee
Sven Mokveld <svenmokveldje@gmail.com>
2026-03-02 22:28:04 +0100

    feat: implement database migrations
    This add database migrations using golang-migrate. This should also be
    installed locally.

--- internal/platform/database/database.go
+++ internal/platform/database/database.go
@@ -2,6 +2,10 @@ package database
 
 import (
 	"GEWIS-Rooster/internal/models"
+	"embed"
+	"errors"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/rs/zerolog/log"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
@@ -9,42 +13,76 @@ import (
 	"os"
 )
 
+//go:embed migrations/*.sql
+var migrationFS embed.FS
+
 func ConnectDB(name string) *gorm.DB {
 	devType := os.Getenv("DEV_TYPE")
 
 	var db *gorm.DB
 	var err error
+	var dsn string
 
 	if devType == "production" {
-		dsn := os.Getenv("DATABASE_DSN")
+		dsn = os.Getenv("DATABASE_DSN")
 		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+		if err != nil {
+			log.Fatal().Msgf("Failed to connect database: %v", err)
+		}
+
+		runDBMigrations(dsn)
 	} else {
+		dsn = "sqlite3://" + name
 		db, err = gorm.Open(sqlite.Open(name), &gorm.Config{})
-	}
 
-	if err != nil {
-		log.Fatal().Msgf("Failed to connect database: %v", err)
-	}
+		if err != nil {
+			log.Fatal().Msgf("Failed to connect database: %v", err)
+		}
 
-	if devType != "production" {
 		db.Exec("PRAGMA foreign_keys = ON")
-	}
 
-	if err := db.AutoMigrate(
-		&models.User{},
-		&models.Organ{},
-		&models.UserOrgan{},
-		&models.Roster{},
-		&models.RosterShift{},
-		&models.RosterAnswer{},
-		&models.SavedShift{},
-		&models.RosterTemplate{},
-		&models.RosterTemplateShift{},
-		&models.RosterTemplateShiftPreference{},
-		&models.ShiftGroup{},
-	); err != nil {
-		panic(err)
+		if err := db.AutoMigrate(
+			&models.User{},
+			&models.Organ{},
+			&models.UserOrgan{},
+			&models.Roster{},
+			&models.RosterShift{},
+			&models.RosterAnswer{},
+			&models.SavedShift{},
+			&models.RosterTemplate{},
+			&models.RosterTemplateShift{},
+			&models.RosterTemplateShiftPreference{},
+			&models.ShiftGroup{},
+		); err != nil {
+			panic(err)
+		}
 	}
 
 	return db
+}
+
+func runDBMigrations(dsn string) {
+	d, err := iofs.New(migrationFS, "db/migrations")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create migration source")
+	}
+
+	m, err := migrate.NewWithSourceInstance("iofs", d, dsn)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to initialize migration")
+	}
+
+	defer func(m *migrate.Migrate) {
+		err, _ := m.Close()
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to close migration source")
+		}
+	}(m)
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatal().Err(err).Msg("Failed to run migrations")
+	}
+
+	log.Info().Msg("Database migrations completed successfully")
 }
---
+++ internal/platform/database/migrations/000001_init.up.sql
@@ -0,0 +1,266 @@
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organs`
+--
+
+CREATE TABLE IF NOT EXISTS `organs` (
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) DEFAULT NULL,
+    `updated_at` datetime(3) DEFAULT NULL,
+    `name` longtext DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_organs_name` (`name`) USING HASH
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rosters`
+--
+
+CREATE TABLE IF NOT EXISTS `rosters` (
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) DEFAULT NULL,
+    `updated_at` datetime(3) DEFAULT NULL,
+    `name` longtext DEFAULT NULL,
+    `values` longtext DEFAULT NULL,
+    `organ_id` bigint(20) UNSIGNED DEFAULT NULL,
+    `date` datetime(3) DEFAULT NULL,
+    `saved` tinyint(1) DEFAULT 0,
+    `template_id` bigint(20) UNSIGNED DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_rosters_organ` (`organ_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roster_answers`
+--
+
+CREATE TABLE IF NOT EXISTS `roster_answers` (
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) DEFAULT NULL,
+    `updated_at` datetime(3) DEFAULT NULL,
+    `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+    `roster_id` bigint(20) UNSIGNED DEFAULT NULL,
+    `roster_shift_id` bigint(20) UNSIGNED DEFAULT NULL,
+    `value` longtext DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `user_answer_idx` (`user_id`,`roster_id`,`roster_shift_id`),
+    KEY `fk_roster_answers_roster_shift` (`roster_shift_id`),
+    KEY `fk_rosters_roster_answer` (`roster_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roster_shifts`
+--
+
+CREATE TABLE IF NOT EXISTS `roster_shifts` (
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) DEFAULT NULL,
+    `updated_at` datetime(3) DEFAULT NULL,
+    `name` longtext DEFAULT NULL,
+    `roster_id` bigint(20) UNSIGNED DEFAULT NULL,
+    `order` bigint(20) UNSIGNED DEFAULT NULL,
+    `shift_group_id` bigint(20) UNSIGNED DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_rosters_roster_shift` (`roster_id`),
+    KEY `fk_roster_shifts_shift_group` (`shift_group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roster_templates`
+--
+
+CREATE TABLE IF NOT EXISTS `roster_templates` (
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) DEFAULT NULL,
+    `updated_at` datetime(3) DEFAULT NULL,
+    `organ_id` bigint(20) UNSIGNED DEFAULT NULL,
+    `name` longtext DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roster_template_shifts`
+--
+
+CREATE TABLE IF NOT EXISTS `roster_template_shifts` (
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) DEFAULT NULL,
+    `updated_at` datetime(3) DEFAULT NULL,
+    `template_id` bigint(20) UNSIGNED DEFAULT NULL,
+    `shift_name` longtext DEFAULT NULL,
+    `shift_group_id` bigint(20) UNSIGNED DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_roster_templates_shifts` (`template_id`),
+    KEY `fk_roster_template_shifts_shift_group` (`shift_group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roster_template_shift_preferences`
+--
+
+CREATE TABLE IF NOT EXISTS `roster_template_shift_preferences` (
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) DEFAULT NULL,
+    `updated_at` datetime(3) DEFAULT NULL,
+    `roster_template_shift_id` bigint(20) UNSIGNED DEFAULT NULL,
+    `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+    `preference` longtext DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_roster_template_shift_preferences_roster_template_shift` (`roster_template_shift_id`),
+    KEY `fk_roster_template_shift_preferences_user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saved_shifts`
+--
+
+CREATE TABLE IF NOT EXISTS `saved_shifts` (
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) DEFAULT NULL,
+    `updated_at` datetime(3) DEFAULT NULL,
+    `roster_id` bigint(20) UNSIGNED DEFAULT NULL,
+    `roster_shift_id` bigint(20) UNSIGNED DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_saved_shifts_roster_shift` (`roster_shift_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shift_groups`
+--
+
+CREATE TABLE IF NOT EXISTS `shift_groups` (
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) DEFAULT NULL,
+    `updated_at` datetime(3) DEFAULT NULL,
+    `organ_id` bigint(20) UNSIGNED DEFAULT NULL,
+    `name` longtext DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `organ_shift_group` (`organ_id`,`name`) USING HASH,
+    KEY `fk_shift_groups_organ` (`organ_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` datetime(3) DEFAULT NULL,
+    `updated_at` datetime(3) DEFAULT NULL,
+    `name` longtext DEFAULT NULL,
+    `gewis_id` bigint(20) UNSIGNED DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_name` (`gewis_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_organs`
+--
+
+CREATE TABLE IF NOT EXISTS `user_organs` (
+    `organ_id` bigint(20) UNSIGNED NOT NULL,
+    `user_id` bigint(20) UNSIGNED NOT NULL,
+    `username` varchar(25) DEFAULT NULL,
+    PRIMARY KEY (`organ_id`,`user_id`),
+    KEY `fk_user_organs_user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_shift_saved`
+--
+
+CREATE TABLE IF NOT EXISTS `user_shift_saved` (
+    `saved_shift_id` bigint(20) UNSIGNED NOT NULL,
+    `user_id` bigint(20) UNSIGNED NOT NULL,
+    PRIMARY KEY (`saved_shift_id`,`user_id`),
+    KEY `fk_user_shift_saved_user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `rosters`
+--
+ALTER TABLE `rosters`
+    ADD CONSTRAINT `fk_rosters_organ` FOREIGN KEY (`organ_id`) REFERENCES `organs` (`id`);
+
+--
+-- Constraints for table `roster_answers`
+--
+ALTER TABLE `roster_answers`
+    ADD CONSTRAINT `fk_roster_answers_roster_shift` FOREIGN KEY (`roster_shift_id`) REFERENCES `roster_shifts` (`id`) ON DELETE CASCADE,
+    ADD CONSTRAINT `fk_rosters_roster_answer` FOREIGN KEY (`roster_id`) REFERENCES `rosters` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `roster_shifts`
+--
+ALTER TABLE `roster_shifts`
+    ADD CONSTRAINT `fk_roster_shifts_shift_group` FOREIGN KEY (`shift_group_id`) REFERENCES `shift_groups` (`id`) ON DELETE SET NULL,
+    ADD CONSTRAINT `fk_rosters_roster_shift` FOREIGN KEY (`roster_id`) REFERENCES `rosters` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `roster_template_shifts`
+--
+ALTER TABLE `roster_template_shifts`
+    ADD CONSTRAINT `fk_roster_template_shifts_shift_group` FOREIGN KEY (`shift_group_id`) REFERENCES `shift_groups` (`id`) ON DELETE SET NULL,
+    ADD CONSTRAINT `fk_roster_templates_shifts` FOREIGN KEY (`template_id`) REFERENCES `roster_templates` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `roster_template_shift_preferences`
+--
+ALTER TABLE `roster_template_shift_preferences`
+    ADD CONSTRAINT `fk_roster_template_shift_preferences_roster_template_shift` FOREIGN KEY (`roster_template_shift_id`) REFERENCES `roster_template_shifts` (`id`) ON DELETE CASCADE,
+    ADD CONSTRAINT `fk_roster_template_shift_preferences_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `saved_shifts`
+--
+ALTER TABLE `saved_shifts`
+    ADD CONSTRAINT `fk_saved_shifts_roster_shift` FOREIGN KEY (`roster_shift_id`) REFERENCES `roster_shifts` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `shift_groups`
+--
+ALTER TABLE `shift_groups`
+    ADD CONSTRAINT `fk_shift_groups_organ` FOREIGN KEY (`organ_id`) REFERENCES `organs` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user_organs`
+--
+ALTER TABLE `user_organs`
+    ADD CONSTRAINT `fk_user_organs_organ` FOREIGN KEY (`organ_id`) REFERENCES `organs` (`id`),
+    ADD CONSTRAINT `fk_user_organs_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `user_shift_saved`
+--
+ALTER TABLE `user_shift_saved`
+    ADD CONSTRAINT `fk_user_shift_saved_saved_shift` FOREIGN KEY (`saved_shift_id`) REFERENCES `saved_shifts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT `fk_user_shift_saved_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
