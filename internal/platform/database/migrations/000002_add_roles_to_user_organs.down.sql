de7edc477c81da43d7bb071df8fd3ebad02684d5
Sven Mokveld <svenmokveldje@gmail.com>
2026-03-10 22:35:39 +0100

    fix: user organ roles migrations
    Some SQL statements in the migrations were wrong.

--- internal/platform/database/migrations/000002_add_roles_to_user_organs.down.sql
+++ internal/platform/database/migrations/000002_add_roles_to_user_organs.down.sql
@@ -1,6 +1,4 @@
 ALTER TABLE user_organs
-ADD COLUMN role VARCHAR(20) not null DEFAULT 'member';
-
-ALTER TABLE user_organs
-    ADD "CONSTRAINT" chk_user_organs_role
+    ADD COLUMN role VARCHAR(20) NOT NULL DEFAULT 'member',
+    ADD CONSTRAINT chk_user_organs_role
         CHECK (role IN ('owner', 'admin', 'member'));
